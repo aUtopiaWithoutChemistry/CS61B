e6f5752f285715ac3da4c000284f662a6c0147d6
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2023-11-26 16:38:53 -0500

    added the test for addFirst method, and pass this test

--- proj1b/tests/ArrayDequeTest.java
+++ proj1b/tests/ArrayDequeTest.java
@@ -10,14 +10,22 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDequeTest {
 
-    // @Test
-    // @DisplayName("ArrayDeque has no fields besides backing array and primitives")
-    // void noNonTrivialFields() {
-    //     List<Field> badFields = Reflection.getFields(ArrayDeque.class)
-    //             .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-    //             .toList();
+     @Test
+     @DisplayName("ArrayDeque has no fields besides backing array and primitives")
+     void noNonTrivialFields() {
+         List<Field> badFields = Reflection.getFields(ArrayDeque.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                 .toList();
 
-    //     assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-    // }
+         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+     }
+
+    @Test
+    public void testAddFirst() {
+        Deque<Integer> al1 = new ArrayDeque();
+        al1.addFirst(3);
+        al1.addFirst(4);
+        assertThat(al1.toList()).containsExactly(null, null, null, null, null, null, 4, 3).inOrder();
+    }
 
 }
