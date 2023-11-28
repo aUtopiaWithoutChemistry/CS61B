dd3e44c8b6edc9b64a044d9f6e3539f4bf575b07
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2023-11-28 17:53:54 -0500

    added test case for iterator and pass the test

--- proj1c/tests/MaxArrayDequeTest.java
+++ proj1c/tests/MaxArrayDequeTest.java
@@ -3,9 +3,22 @@ import deque.Deque;
 import deque.ArrayDeque;
 import deque.LinkedListDeque;
 
+import java.util.Iterator;
+
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 public class MaxArrayDequeTest {
+    @Test
+    public void arrayListDequeIteratorTest() {
+        Deque<Integer> al1 = new ArrayDeque();
+        al1.addLast(4); // [4, null, null, null, null, null, null, null]
+        al1.addLast(5); // [4,    5, null, null, null, null, null, null]
+        al1.addLast(6); // [4,    5,    6, null, null, null, null, null]
+
+        for(int i : al1) {
+            System.out.println(i);
+        }
+    }
 
 }
