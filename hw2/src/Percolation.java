7cb7d6c186b9ab919ddcf5a56d75ff854f229d80
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2023-12-04 11:58:39 -0500

    changed the percolates method to have the time complexity of  O(1) and pass the test

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -10,17 +10,24 @@ public class Percolation {
     private int lastFullElement = -1;
 
     public Percolation(int N) {
-        // TODO: Fill in this constructor.
+
         if (N > 0) {
+            // O(N^2)
             uf = new WeightedQuickUnionUF(N * N);
             size = N;
 
             // use a 2D array to track the status, initialized as blocked
             status = new int[N][N];
+            // O(N^2)
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
                     status[i][j] = -1;
                 }
+            }
+
+            // connect all the bottom brick together in the djs
+            for (int i = 1; i < size; i++) {
+                uf.union((size * (size - 1)), (i + size * (size - 1)));
             }
 
             //System.out.println(status);
@@ -31,7 +38,8 @@ public class Percolation {
     }
 
     public void open(int row, int col) {
-        // TODO: Fill in this method.
+
+        // O(1)
         if (row < size && col < size) {
             status[row][col] = 0;
             openedSites += 1;
@@ -57,33 +65,6 @@ public class Percolation {
                 }
                 lastFullElement = col;
             }
-
-            //
-            //if ()
-
-//            // check four neighbor of this is full,
-//            // if they are full, and this brick is open, then this is full
-//            if (((row - 1 >= 0 && status[row - 1][col] == 1)
-//                    || (row + 1 < size && status[row + 1][col] == 1)
-//                    || (col - 1 >= 0 && status[row][col - 1] == 1)
-//                    || (col + 1 < size && status[row][col + 1] == 1))
-//                    && status[row][col] == 0) {
-//                status[row][col] = 1;
-//            }
-//
-//            // check four neighbor of this is full,
-//            // if they are not full but open, and this brick is full, then they are full
-//            if (status[row][col] == 1) {
-//                if (row - 1 >= 0 && status[row - 1][col] == 0) {
-//                    open(row - 1, col);
-//                } else if (row + 1 < size && status[row + 1][col] == 0) {
-//                    open(row + 1, col);
-//                } else if (col - 1 >= 0 && status[row][col - 1] == 0) {
-//                    open(row, col - 1);
-//                } else if (col + 1 < size && status[row][col + 1] == 0) {
-//                    open(row, col + 1);
-//                }
-//            }
         }
         else {
             throw new IndexOutOfBoundsException("row and col should be lower or equals to " + size);
@@ -91,7 +72,8 @@ public class Percolation {
     }
 
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
+
+        // O(1)
         boolean isOpen = false;
         if (row < size && col < size) {
             if (status[row][col] == 0 || status[row][col] == 1) {
@@ -104,7 +86,8 @@ public class Percolation {
     }
 
     public boolean isFull(int row, int col) {
-        // TODO: Fill in this method.
+
+        // O(1)
         boolean isFull = false;
         if (row < size && col < size && row >= 0 && col >= 0) {
             if (lastFullElement != -1) {
@@ -123,23 +106,22 @@ public class Percolation {
     }
 
     public int numberOfOpenSites() {
-        // TODO: Fill in this method.
+
+        // O(1)
         return openedSites;
     }
 
     public boolean percolates() {
-        // TODO: Fill in this method.
-        for (int i = 0; i < size; i++) {
-            if (lastFullElement != -1){
-                if (uf.connected((i + size * (size - 1)), lastFullElement)) {
-                    return true;
-                }
+
+        // check if the first node of the bottom connected to the full set
+        // because all the bottom are connected when initialization
+        // O(1)
+        if (lastFullElement != -1){
+            if (uf.connected(size * (size - 1) + 1, lastFullElement)) {
+                return true;
             }
         }
         return false;
     }
-
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
 
 }
