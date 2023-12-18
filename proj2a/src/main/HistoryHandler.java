a1b56f74d160b8b71aadce0e1d7e9cddbc250665
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2023-12-18 16:22:49 -0500

    finished HistoryHandler and the main function

--- proj2a/src/main/HistoryHandler.java
+++ proj2a/src/main/HistoryHandler.java
@@ -1,5 +1,40 @@
 package main;
 
-public class HistoryHandler {
+import browser.NgordnetQuery;
+import browser.NgordnetQueryHandler;
+import ngrams.NGramMap;
+import ngrams.TimeSeries;
+import org.knowm.xchart.XYChart;
+import plotting.Plotter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryHandler extends NgordnetQueryHandler {
+
+    private NGramMap ngm;
+    public HistoryHandler(NGramMap ngMap) {
+        ngm = ngMap;
+    }
+
+    @Override
+    public String handle(NgordnetQuery q) {
+
+        List<String> words = q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+        ArrayList<TimeSeries> lts = new ArrayList<>();
+        ArrayList<String> labels = new ArrayList<>();
+
+        for(String word:words) {
+            TimeSeries ts = ngm.weightHistory(word, startYear, endYear);
+            lts.add(ts);
+            labels.add(word);
+        }
+
+        XYChart chart = Plotter.generateTimeSeriesChart(labels, lts);
+        String encodedImage = Plotter.encodeChartAsString(chart);
+
+        return encodedImage;
+    }
 }
